1b43a6e6a291209cee20d9404aae6678be595b09
RISSOULI-Mohammed <simo.rissouli@hotmail.com>
2018-03-31 13:31:29 +0100

    Social Sharing

--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams, ToastController, ActionSheetController, ModalController } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { Comment } from '../../shared/comment';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 import { FavoriteProvider } from '../../providers/favorite/favorite';
 import { Title } from '@angular/platform-browser';
@@ -30,7 +31,8 @@ export class DishdetailPage {
     private favoriteservice: FavoriteProvider,
     private toastCtrl: ToastController,
     private actioSheetCtrl: ActionSheetController,
-    private modalCtrl: ModalController) {
+    private modalCtrl: ModalController,
+    private socialSharing: SocialSharing) {
     this.dish = navParams.get('dish');
     this.favorite = favoriteservice.isFavorite(this.dish.id);
     this.numcomments = this.dish.comments.length;
@@ -49,7 +51,7 @@ export class DishdetailPage {
       duration: 3000}).present();
   }
 
-  moreActions(){
+  presentActionSheet(){
     let actionSheet = this.actioSheetCtrl.create({
       title: 'Select Actions',
       buttons: [
@@ -64,6 +66,20 @@ export class DishdetailPage {
             let modal = this.modalCtrl.create(CommentPage);
             modal.present();
             modal.onDidDismiss(comment => {this.dish.comments.push(comment)})
+          }
+        },{
+          text: 'Share via Facebook',
+          handler: () => {
+            this.socialSharing.shareViaFacebook(this.dish.name + ' -- ' + this.dish.description, this.BaseURL + this.dish.image, '')
+              .then(() => console.log('Posted successfully to Facebook'))
+              .catch(() => console.log('Failed to post to Facebook'));
+          }
+        },{
+          text: 'Share via Twitter',
+          handler: () => {
+            this.socialSharing.shareViaTwitter(this.dish.name + ' -- ' + this.dish.description, this.BaseURL + this.dish.image, '')
+              .then(() => console.log('Posted successfully to Twitter'))
+              .catch(() => console.log('Failed to post to Twitter'));
           }
         },{
           text: 'Cancel',
